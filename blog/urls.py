e9d55b7e90740805168fb43bd4922a52000e9c5f
Serj Ilyashenko <serj.ilyashenko@gmail.com>
2017-01-08 22:26:03 +0300

    first prototype

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'posts/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'posts/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit')
+]
