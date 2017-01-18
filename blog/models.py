b2c580b5f402feeb6061f60f74e32903ee745c62
Serj Ilyashenko <serj.ilyashenko@gmail.com>
2017-01-19 00:17:48 +0300

    approve comments remove comments

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,9 @@ class Post(models.Model):
         null=True,
     )
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
